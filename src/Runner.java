395fa7b9263c29008334f8d78769b113137a184f
Kuryakov <thedropone@gmail.com>
2016-11-09 03:22:20 +0300

    Peace of 3 task

--- src/Runner.java
+++ src/Runner.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,23 +7,23 @@ import java.util.regex.Pattern;
  * Created by Semen on 08-Nov-16.
  */
 public class Runner {
-    /*Задание. Родственные слова.
+    /*Задание. Разработать консольное приложение, выполняющее следующие действия:
 
-Назовем два слова родственными, если одно может быть получено из другого циклическим сдвигом на несколько позиций.
+    1. На вход поступает текстовый файл input.in.
+    2. На 4 балла. Отсортировать строки текста по возрастанию количества символов в строке.
+        Результат записать в выходной файл output1.out.
+    3. На 5 баллов (задание 2 + задание 3). Отсортировать символы в каждой строке текста по
+        возрастанию кодов символов. Результат записать в выходной файл output2.out.
+    4. На 6-7 баллов (задание 2 + задание 3 + задание 4). Отсортировать слова в каждой строке
+        файла по алфавиту. Регистр не имеет значения. Разделители: пробел, точка, запятая, точка
+        с запятой. Результат записать в выходной файл output3.out.
 
-Например:  камыш – мышка
+    Дополнительные баллы (за каждый пункт 1 балл):
 
-Во входном файле input.txt находится текст.
-Разделители: пробел, точка, запятая, точка с запятой.
-Исходный файл может быть пустым.
-В файле может быть несколько строк.
-Регистр не имеет значения.
-
-В выходной файл output.txt вывести все группы родственных слов (слова одной группы – через пробел в одну строку, слова следующей группы – с новой строки).
-Если слово не имеет родственных слов, то оно будет в своей группе одно.
-
-При проходе по коллекции хотя бы один раз использовать итератор.
-*/
+        1. Использовать коллекцию.
+        2. Использовать итератор.
+        3. Реализовать компараторы для сортировки.
+    */
     public static void main(String[] args) {
         try {
             kr1();
@@ -98,6 +95,45 @@ public class Runner {
         }
         return array;
     }
+
+
+    private static void kr2() {
+
+    }
+
+    private static void kr3() {
+        String pathInput = "input.in";
+        String pathOutput1 = "output1.out";
+
+
+        List<String> listOfStrings = readListOfStrings(pathInput);
+        // task 1. 4 points
+        Set<String> setOfStrings = new TreeSet<>();
+        setOfStrings = (Set<String>) listOfStrings;
+        for (String setOfString : setOfStrings) {
+            System.out.println(setOfString);
+        }
+        writeToFile(pathOutput1,setOfStrings.iterator());
+    }
+
+    private static void writeToFile(String filename, Iterator<?> iterator) {
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(new File(filename));
+            while (iterator.hasNext()) {
+                fw.write((String) iterator.next());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fw.close();
+            } catch (IOException | NullPointerException ex) {
+                System.out.println("Stream didn't closed or not exists. Reading from file failed. ");
+            }
+        }
+    }
+
 }
 
 
